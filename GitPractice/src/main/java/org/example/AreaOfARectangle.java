704ed667c54a88dc7ce2e5679d3f9742ae6284a7
Chandupa Marapana <136840067+chandupamarapana@users.noreply.github.com>
2024-07-21 19:47:36 +0530

    Add initial Java code

---
+++ GitPractice/src/main/java/org/example/AreaOfARectangle.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class AreaOfARectangle {
+        public static void main(String[] args) {
+            Scanner Scan = new Scanner(System.in);
+            boolean validInput = false;
+            while (!validInput) {
+                try {
+
+                    System.out.print("Enter the length for the rectangle: ");
+                    int length = Scan.nextInt();
+                    System.out.print("Enter the width for the rectangle: ");
+                    int breath = Scan.nextInt();
+
+                    int Area = length * breath;
+
+                    System.out.println("the area of the rectangle is :" + Area);
+                    validInput = true;
+                } catch (InputMismatchException e) {
+                    System.out.println("invalid input pplease enter integers only: ");
+                    Scan.nextLine();
+                }
+
+            }
+
+        }
+    }
+
