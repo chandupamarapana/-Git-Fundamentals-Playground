a718e35446cdef89690d06ce60dae109f7a4d1c2
Chandupa Marapana <136840067+chandupamarapana@users.noreply.github.com>
2024-07-21 20:30:24 +0530

    this is a new commit for branch 2

--- GitPractice/src/main/java/org/example/AreaOfARectangle.java
+++ GitPractice/src/main/java/org/example/AreaOfARectangle.java
@@ -20,6 +20,8 @@ public class AreaOfARectangle {
                     System.out.println("the area of the rectangle is :" + Area);
                     validInput = true;
                     System.out.println("this change is after a new branch, branch-1");
+
+                    System.out.println("this change is after a new branch, branch-2");
                 } catch (InputMismatchException e) {
                     System.out.println("invalid input pplease enter integers only: ");
                     Scan.nextLine();
